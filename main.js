4387a228b80477ac71ee5ed03a4d547f0c67ed19
KaterinaB10 <katerina.b10.kodehode@gmail.com>
2024-01-21 17:00:44 +0100

    task 4 done

--- main.js
+++ main.js
@@ -111,7 +111,8 @@ Example 2: ["One", "Two", "Three", "Four", "Five", "Six"] should return
 ["Two", "Three", "Four", "Five"]
 ******************************************************************************/
 
-export function arrayTrimmer() {
+export function arrayTrimmer(arr) {
+  return arr.slice(1, -1);
   //your code here
 }
 
